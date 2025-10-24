b8ed24189b0e873897d122759a6b0e684b8605a0
Bryan12sd <bryanzeta17@gmail.com>
2025-10-23 23:04:39 -0500

    feat: add changes for rules

--- app/src/main/java/com/example/reservasalasapp/ui/rules/RuleFragment.kt
+++ app/src/main/java/com/example/reservasalasapp/ui/rules/RuleFragment.kt
@@ -6,21 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.reservasalasapp.R
-
+import android.webkit.WebView
+import android.webkit.WebViewClient
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RuleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RuleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,27 +31,15 @@ class RuleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rule, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_rule, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RuleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RuleFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        webView = view.findViewById(R.id.webviewRule)
+        webView.webViewClient = WebViewClient() // evita que se abra navegador externo
+        webView.settings.javaScriptEnabled = true // habilitar JS si la web lo necesita
+
+        // Cargar URL, por ejemplo
+        webView.loadUrl("https://www.egafutura.com/glosario/reservas")
+
+        return view
     }
 }
